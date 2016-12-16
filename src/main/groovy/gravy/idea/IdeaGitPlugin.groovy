5743ee27ab874ee4f8a2ace313cdf841add23307
Javier Campanini <jcampanini@palantir.com>
2016-12-16 16:39:42 +1100

    initial commit

---
+++ src/main/groovy/gravy/idea/IdeaGitPlugin.groovy
@@ -0,0 +1,12 @@
+package gravy.idea
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class IdeaGitPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
